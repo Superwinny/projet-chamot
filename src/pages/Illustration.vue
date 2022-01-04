6f76c097c9ac710d338d5dced0551565d31f47d3
Marco Tribuzio <marco.tribuz@gmail.com>
2022-01-04 14:59:59 +0100

    Bugfix

--- src/pages/Illustration.vue
+++ src/pages/Illustration.vue
@@ -1,115 +1,113 @@
 <template>
-  <v-row>
-    <!-- Ligne une  -->
-    <v-col cols="12" md="4">
-      <v-card class="mx-auto" max-width="auto">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="250px"
-          width="auto"
-          @click="showDialog()"
-        ></v-img>
-
-        <v-card-title> Top western road trips </v-card-title>
-
-        <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-      </v-card>
-    </v-col>
-    <v-col cols="12" md="4">
-      <v-card class="mx-auto" max-width="auto">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="250px"
-          width="auto"
-        ></v-img>
-
-        <v-card-title> Top western road trips </v-card-title>
-
-        <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-      </v-card>
-    </v-col>
-    <v-col cols="12" md="4">
-      <v-card class="mx-auto" max-width="auto">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="250px"
-          width="auto"
-        ></v-img>
-
-        <v-card-title> Top western road trips </v-card-title>
-
-        <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-      </v-card>
-    </v-col>
-    <!-- Ligne deux -->
-    <v-col cols="12" md="4">
-      <v-card class="mx-auto" max-width="auto">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="250px"
-          width="auto"
-        ></v-img>
-
-        <v-card-title> Top western road trips </v-card-title>
-
-        <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-      </v-card>
-    </v-col>
-    <v-col cols="12" md="4">
-      <v-card class="mx-auto" max-width="auto">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="250px"
-          width="auto"
-        ></v-img>
-
-        <v-card-title> Top western road trips </v-card-title>
-
-        <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-      </v-card>
-    </v-col>
-    <v-col cols="12" md="4">
-      <v-card class="mx-auto" max-width="auto">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="250px"
-          width="auto"
-        ></v-img>
-
-        <v-card-title> Top western road trips </v-card-title>
-
-        <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-      </v-card>
-    </v-col>
-    <!-- Dialogue -->
-
-    <v-dialog v-model="dialog" persistent max-width="50%"   >
-      <template>
-        <v-col cols="12" md="6" sm="4">
-          <v-card class="mx-auto" max-width="auto">
-            <v-img
+  <v-container>
+    <v-row>
+      <!-- Ligne une  -->
+      <v-col cols="12" md="4">
+        <v-card class="mx-auto" max-width="auto">
+          <v-img
               src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-              height="300px"
+              height="250px"
               width="auto"
               @click="showDialog()"
-            ></v-img>
-            
-          </v-card>
-        </v-col>
-     
-        <v-col cols="12" md="6" sm="4">
-           <v-card class="mx-auto" max-width="auto">
-          <v-card-title> Top western road trips </v-card-title>
+          ></v-img>
 
-          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-          
-          </v-card>
+          <v-card-title> Top western road trips</v-card-title>
 
-        </v-col>
-        
-      </template>
-    </v-dialog>
-  </v-row>
+          <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card class="mx-auto" max-width="auto">
+          <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="250px"
+              width="auto"
+          ></v-img>
+
+          <v-card-title> Top western road trips</v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card class="mx-auto" max-width="auto">
+          <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="250px"
+              width="auto"
+          ></v-img>
+
+          <v-card-title> Top western road trips</v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
+        </v-card>
+      </v-col>
+      <!-- Ligne deux -->
+      <v-col cols="12" md="4">
+        <v-card class="mx-auto" max-width="auto">
+          <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="250px"
+              width="auto"
+          ></v-img>
+
+          <v-card-title> Top western road trips</v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card class="mx-auto" max-width="auto">
+          <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="250px"
+              width="auto"
+          ></v-img>
+
+          <v-card-title> Top western road trips</v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-card class="mx-auto" max-width="auto">
+          <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="250px"
+              width="auto"
+          ></v-img>
+
+          <v-card-title> Top western road trips</v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
+        </v-card>
+      </v-col>
+
+      <!-- Dialogue -->
+
+      <v-dialog v-model="dialog" max-width="50%">
+        <v-card max-width="auto">
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="6">
+                  <v-img
+                      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                      height="300px"
+                      width="auto"
+                  ></v-img>
+                </v-col>
+                <v-col cols="6">
+                  <v-card-title> Top western road trips</v-card-title>
+                  <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -131,11 +129,5 @@ export default {
 </script>
 
 <style>
-.row {
-  margin-top: 150px;
-  margin-left: 10%;
-  margin-right: 10%;
-  max-width: 100%;
-  margin-bottom: 150px;
-}
+
 </style>
