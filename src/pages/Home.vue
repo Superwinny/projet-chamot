fba7d2f0e738d0a85a8d3130991f5cc3935b8926
Dos Santos Davide <85991452+Superwinny@users.noreply.github.com>
2022-01-20 16:50:16 +0100

    commit

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <v-row  id="scrolling-techniques-4">
+    <v-row id="scrolling-techniques-4">
       <v-col cols="12" md="4">
         <v-card class="mx-auto" max-width="auto">
           <v-img
@@ -12,14 +12,32 @@
       </v-col>
       <v-col cols="12" md="6" class="home">
         <v-card class="d-flex align-center mb-6" flat height="100%">
-        <div class="descriptions">
-          <p>Davideaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          <p>Davide</p>
-          <p>Davide</p>
-          <p>Davide</p>
-          
-        </div>
-        </v-card> 
+          <div class="descriptions">
+            <p>
+              POLYDESIGNER n.n (du grec poly - plusieurs et du latin designare -
+              marquer d'un signe, dessiner, indiquer):
+            </p>
+            <p>
+              Le polydesigner traduit les souhaits des clients sous forme
+              d'esquisses, de maquettes, de concepts visuels afin d'illustrer
+              leur projet. Il met à profit ses connaissances en matière de
+              design 2D ou 3D, de planification, de réalisation et de mise en
+              place pour son client afin de créer une unité visuelle dans son
+              projet.
+            </p>
+            <p>
+              Le polydesigner est à l'aise dans la création d'identité visuelle
+              et de conception d'espace 3D (stands, shop-in-shop, vitrines...).
+              Ses lieux de travail sont le plus souvent les agences de
+              communication, les grands magasins ou les musées.
+            </p>
+            <p>
+              Les domaines d'activité principaux du polydesigner sont la
+              communication visuelle, le visual merchandising, la scénographie
+              et la muséographie.
+            </p>
+          </div>
+        </v-card>
       </v-col>
     </v-row>
   </div>
@@ -32,7 +50,7 @@ export default {
 };
 </script>
 <style>
-.home{
+.home {
   margin-top: 50px;
   margin-bottom: 50px;
 }
